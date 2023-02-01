babaf0b96a94a2671cc0b40c31f2118d4338e8c6
POOJA HARIHAR <88924201+poojaharihar03@users.noreply.github.com>
2023-02-01 08:06:28 +0530

    updated fifa.sql

--- db/fifa.sql
+++ db/fifa.sql
@@ -1,11 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 4.8.3
--- https://www.phpmyadmin.net/
--- Host: 127.0.0.1:3306
--- Generation Time: Dec 09, 2018 at 06:23 PM
--- Server version: 5.7.23
--- PHP Version: 7.2.10
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
